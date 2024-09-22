bc16b9138506abab7aeb4351ac3c8c3323752c8f
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2024-09-22 22:04:31 +0000

    Update console script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vm-video-downloader
-Download Vimeo videos easy v0.1
+Download Vimeo videos easy v0.2<br/>
+Authored by: [limontec](https://github.com/limontec)<br/>
+Modified by: [Bartek20](https://github.com/Bartek20)
 
 # Usage with slow download
 Click on the "Open in Colab" button.
@@ -8,14 +10,30 @@ Click on the "Open in Colab" button.
 # Usage with fast download
 
 ### For Windows:
-1. Download ffmpeg https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip 
-2. Put the script vm-video-downloader.py in the folder
-   1. C:\user\Downloads\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin
-4. Run the script with: python vm-video-downloader.py
+1. Download [ffmpeg](https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip)
+2. Extract downloaded zip file 
+2. Put the script **vm-video-downloader.py** in the extracted folder (to the **bin** directory)
+4. Run the script (shown below)
 
 ### For Linux:
 1. sudo apt install ffmpeg
-2. Run the script vm-video-downloader.py with: python vm-video-downloader.py
+2. Run the script (shown below)
+
+## Running script
+```bash
+# Provide required data while running script
+python vm-video-downloader.py
+```
+```bash
+# Provide required data before running script
+python vm-video-downloader.py "[master|playlist].json url" "FILENAME"
+```
+```bash
+# Provide list of urls
+# List has following syntax:
+# PLAYLIST_URL;FILENAME
+python vm-video-downloader.py -p "list path"
+```
 
 # How to find the playlist.json?
 Right click on the video > Inspect > Click on the Network tab > Play the video > Right click on "playlist.json?omit=..." > Copy the Request URL.
--- vm-video-downloader.py
+++ vm-video-downloader.py
@@ -3,6 +3,8 @@ import sys
 import base64
 import requests
 import subprocess
+from tqdm import tqdm
+
 #For Windows:
 #Download ffmpeg https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip
 #Put the script in the folder: C:\user\Downloads\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin
@@ -10,67 +12,85 @@ import subprocess
 #sudo apt install ffmpeg
 #Run the script with: python vm-video-downloader.py
 #Inspired by https://gist.github.com/alexeygrigorev/a1bc540925054b71e1a7268e50ad55cd?permalink_comment_id=5097364#gistcomment-5097364
-#Script v0.1
-from tqdm import tqdm
+#Script v0.2
 
-url = input('enter [master|playlist].json url: ')
-file_name = input('enter output name: ')
-
-if 'master.json' in url:
-    url = url[:url.find('?')] + '?query_string_ranges=1'
-    url = url.replace('master.json', 'master.mpd')
-    print(url)
-    subprocess.run(['youtube-dl', url, '-o', file_name])
-    sys.exit(0)
-
-
-def download(what, to, base):
-    print('saving', what['mime_type'], 'to', to)
+class VimeoDownloader:
+  def __init__(self, url, filename):
+    if 'master.json' in url:
+      url = url[:url.find('?')] + '?query_string_ranges=1'
+      url = url.replace('master.json', 'master.mpd')
+    self.url = url
+    if not filename.endswith('.mp4'):
+      filename += '.mp4'
+    self.filename = filename
+  def fetch(self, what, to, base):
     with open(to, 'wb') as file:
-        init_segment = base64.b64decode(what['init_segment'])
-        file.write(init_segment)
+      init_segment = base64.b64decode(what['init_segment'])
+      file.write(init_segment)
+      for segment in tqdm(what['segments'], desc=to.replace('.mp4', '').upper(), bar_format='{desc}: {percentage:3.0f}% |{bar}| [{n_fmt}/{total_fmt}] '):
+        segment_url = base + segment['url']
+        resp = requests.get(segment_url, stream=True)
+        if resp.status_code != 200:
+          print('Segment not found:', segment_url)
+          break
+        for chunk in resp:
+            file.write(chunk)
+  def download(self):
+    if 'master.mpd' in self.url:
+      subprocess.run(['youtube-dl', self.url, '-o', self.filename])
+      return
+    base_url = self.url[:self.url.rfind('/', 0, -26) + 1]
+    content = requests.get(self.url).json()
+    vid_heights = [(i, d['height']) for (i, d) in enumerate(content['video'])]
+    vid_idx, _ = max(vid_heights, key=lambda _h: _h[1])
+    audio_quality = [(i, d['bitrate']) for (i, d) in enumerate(content['audio'])]
+    audio_idx, _ = max(audio_quality, key=lambda _h: _h[1])
+    video = content['video'][vid_idx]
+    audio = content['audio'][audio_idx]
+    base_url = base_url + content['base_url']
+    video_tmp_file = 'video.mp4'
+    audio_tmp_file = 'audio.mp4'
+    self.fetch(video, video_tmp_file, base_url + video['base_url'])
+    self.fetch(audio, audio_tmp_file, base_url + audio['base_url'])
+    command = ["ffmpeg", "-i", audio_tmp_file, "-i", video_tmp_file, "-c", "copy", self.filename]
+    try:
+      result = subprocess.run(command, capture_output=True, text=True, check=True)
+      print(result.stdout)
+    except subprocess.CalledProcessError as e:
+      print(f"Error: {e}")
+      print(f"Output error: {e.stderr}")
+    os.remove(video_tmp_file)
+    os.remove(audio_tmp_file)
 
-        for segment in tqdm(what['segments']):
-            segment_url = base + segment['url']
-            resp = requests.get(segment_url, stream=True)
-            if resp.status_code != 200:
-                print('not 200!')
-                print(segment_url)
-                break
-            for chunk in resp:
-                file.write(chunk)
-    print('done')
+DOWNLOAD_MODE = "PLAYLIST" if len(sys.argv) > 1 and sys.argv[1] == '-p' else "SINGLE"
 
+if DOWNLOAD_MODE == 'SINGLE':
+    try: DATA_URL = sys.argv[1]
+    except: DATA_URL = input('Enter [master|playlist].json url: ')
+    try: OUTPUT_FILENAME = sys.argv[2]
+    except: OUTPUT_FILENAME = input('Enter output filename: ')
 
-file_name += '.mp4'
-base_url = url[:url.rfind('/', 0, -26) + 1]
-content = requests.get(url).json()
+data = []
+if DOWNLOAD_MODE == "SINGLE":
+  data.append((DATA_URL, OUTPUT_FILENAME))
+else:
+  if len(sys.argv) < 3:
+    print('No playlist data provided. Exiting...')
+    sys.exit(1)
+  if not os.path.exists(sys.argv[2]):
+    print('Provided playlist file does not exist. Exiting...')
+    sys.exit(1)
+  with open(sys.argv[2], 'r') as file:
+    for i, line in enumerate(file.readlines(), start=1):
+      videoData = [arg.strip() for arg in line.split(';')]
+      if len(videoData) != 2 or videoData[0] == '' or videoData[1] == '':
+        print('Incorrect syntax in line', i)
+        continue
+      data.append(tuple(videoData))
 
-vid_heights = [(i, d['height']) for (i, d) in enumerate(content['video'])]
-vid_idx, _ = max(vid_heights, key=lambda _h: _h[1])
-
-audio_quality = [(i, d['bitrate']) for (i, d) in enumerate(content['audio'])]
-audio_idx, _ = max(audio_quality, key=lambda _h: _h[1])
-
-video = content['video'][vid_idx]
-audio = content['audio'][audio_idx]
-base_url = base_url + content['base_url']
-
-video_tmp_file = 'video.mp4'
-audio_tmp_file = 'audio.mp4'
-
-download(video, video_tmp_file, base_url + video['base_url'])
-download(audio, audio_tmp_file, base_url + audio['base_url'])
-
-command = ["ffmpeg", "-i", audio_tmp_file, "-i", video_tmp_file, "-c", "copy", file_name]
-
-try:
-    print("Joining video and audio...")
-    result = subprocess.run(command, capture_output=True, text=True, check=True)
-    print(result.stdout)  # Exibe a saída do comando
-except subprocess.CalledProcessError as e:
-    print(f"Error: {e}")
-    print(f"Output error: {e.stderr}")
-
-os.remove(video_tmp_file)
-os.remove(audio_tmp_file)
+for DATA_URL, OUTPUT_FILENAME in data:
+  if DATA_URL == '' or OUTPUT_FILENAME == '':
+    continue
+  print(f"Downloading {OUTPUT_FILENAME}...")
+  downloader = VimeoDownloader(DATA_URL, OUTPUT_FILENAME)
+  downloader.download()
